6762dc6c1ff12287595e994058e1574c12560ed4
diego damasco <diegodamasco5665@gmail.com>
2025-07-03 12:33:44 -0600

    pulsense front

--- backend-login/models/Usuario.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const usuarioSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-});
-
-module.exports = mongoose.model('Usuario', usuarioSchema);
