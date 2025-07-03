f455ae2a8dd7a3917cab5a19a03a03c64be2aee4
diego damasco <diegodamasco5665@gmail.com>
2025-07-02 23:02:56 -0600

    1.0

--- backend-login/index.js
+++ backend-login/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Variables de entorno
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // Conexión a MongoDB
