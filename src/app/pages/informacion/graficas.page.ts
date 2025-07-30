a73f36d4c311c213d9d2ff031ef6b16c2e42e898
diego damasco <diegodamasco5665@gmail.com>
2025-07-30 00:12:08 -0600

    sin ion exesivos

--- src/app/pages/informacion/graficas.page.ts
+++ src/app/pages/informacion/graficas.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader,IonButton } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,7 +9,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './graficas.page.html',
   styleUrls: ['./graficas.page.scss'],
   standalone: true,
-  imports: [IonContent,RouterModule, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [IonContent,RouterModule, IonHeader, CommonModule, FormsModule, IonButton]
 })
 export class GraficasPage {
 
--- src/app/pages/iot/iot.page.ts
+++ src/app/pages/iot/iot.page.ts
@@ -1,7 +1,7 @@
 import { Component,  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,7 +9,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './iot.page.html',
   styleUrls: ['./iot.page.scss'],
   standalone: true,
-  imports: [IonContent,RouterModule, IonHeader,IonCol, IonRow, IonTitle, IonToolbar, CommonModule, FormsModule, IonGrid]
+  imports: [IonContent,RouterModule, IonHeader,IonCol, IonRow, CommonModule, FormsModule, IonGrid]
 })
 export class IotPage  {
 
