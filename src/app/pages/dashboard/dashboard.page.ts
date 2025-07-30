b3d173d3d8d24e7ed0e2edec5d19da2f4bf21feb
diego damasco <diegodamasco5665@gmail.com>
2025-07-30 02:46:08 -0600

    css media query button

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { AlertController, IonContent } from '@ionic/angular/standalone';
+import { AlertController, IonContent, IonHeader } from '@ionic/angular/standalone';
 import { AuthService } from '../../services/auth.service';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, IonContent, RouterModule],
+  imports: [CommonModule, IonContent, RouterModule, IonHeader],
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss']
 })
